93c7a4459d205e1f18a91f2afd5d853e4178f802
SV-ta <takumi@vividi.fi>
2022-07-25 20:34:16 +0300

    created separate module for Course

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,34 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) =>{
-  return(
-    <div>
-      {parts.map(part =>
-          <Part key={part.id} part={part}/>
-        )}
-    </div>
-  )
-}
-
-  const Course = ({course, parts}) =>{
-    const initialValue = 0
-    const total = parts.reduce((s, p)=> s + p.exercises, initialValue)
-    return(
-      <>
-      <Header course={course}/>
-      <Content parts={parts}/>
-      <Total sum={total}/>
-    </>
-    )
-  }
-
+import Course from './components/Course';
 const App = () => {
   const courses = [
     {
