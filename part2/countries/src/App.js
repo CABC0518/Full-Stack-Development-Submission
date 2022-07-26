3d11f0f62f30b3c76c5dc90d6c7c8a4b7a2ba0ce
SV-ta <takumi@vividi.fi>
2022-07-26 11:50:52 +0300

    step 2.12 done

---
+++ part2/countries/src/App.js
@@ -0,0 +1,82 @@
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+
+const Search = ({handleSearch, userInput}) => {
+  return(
+    <div>
+      find countries <input onChange={handleSearch} value={userInput}></input>
+    </div>
+  )
+}
+
+const Countries = ({countriesToShow}) =>{
+  if(countriesToShow.length > 10){
+    return(
+      <p>Too many matches, specify another filter</p>
+    )
+  }else if(countriesToShow.length === 1){
+    const onlyCountry = countriesToShow[0]
+    return(
+      <>
+        <h1>{onlyCountry.name.official}</h1>
+        <br></br>
+        <p>Capital: {onlyCountry.capital[0]}</p>
+        <p>Area: {onlyCountry.area}</p>
+        <h3>Languages: </h3>
+        <ul>
+        {Object.values(onlyCountry.languages).map(lang => <li key={lang}>{lang}</li>)}
+        </ul>
+        <img src={onlyCountry.flags.png}></img>
+      </>
+
+    )
+  }else{
+    return(
+      <div>
+        {
+          countriesToShow.map(
+            country => <p key={country.name.official}>{country.name.official}</p>
+          )
+        }
+      </div>
+    )
+  }
+}
+// country.name.official 
+
+const App = ()=>{
+  const [userInput, setUserInput] = useState('')
+  const [countries, setCountries] = useState([])
+  const [countriesToShow, setCountriesToShow] = useState([])
+  const countriesURL = "https://restcountries.com/v3.1/all"
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(countriesURL)
+      .then(response =>{
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleSearch = (e) =>{
+    setUserInput(e.target.value)
+    // create a list of countries based on user input 
+    setCountriesToShow(
+      countries.filter(
+        country =>
+         country.name.official.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    )
+   }
+  return(
+    <div>
+      <p>*Use an official name of a country to search</p>
+      <Search userInput={userInput} handleSearch={handleSearch}/>
+      <Countries countriesToShow={countriesToShow} />
+    </div>
+  )
+}
+
+export default App;
