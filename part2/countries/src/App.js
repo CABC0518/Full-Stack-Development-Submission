aa65a4fb531c89effee320e613a8260f4b7398f5
SV-ta <takumi@vividi.fi>
2022-07-28 09:41:53 +0300

    weather information added to a country info

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -9,6 +9,44 @@ const Search = ({handleSearch, userInput}) => {
   )
 }
 
+const Weather = ({country}) =>{
+
+  const api_key = process.env.REACT_APP_API_KEY
+  const [wind, setWind] = useState("")
+  const [temp, setTemp] = useState("")
+  const capital = country.capital[0]
+  const geoLocationURL = `http://api.openweathermap.org/geo/1.0/direct?q=${capital}&limit=5&appid=${api_key}`
+  
+  // I could've got latitude and longtide from Countries API response. However I didn't notice and retirieve them 
+  // from anothre API instead based on capital of a country.
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(geoLocationURL)
+      .then(response =>{
+        console.log(response.data)
+        console.log('promise fulfilled for geo api')
+        const lon = response.data[0].lon
+        const lat = response.data[0].lat
+        const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`
+        return axios.get(weatherURL)
+      })
+      .then( response=> {
+        console.log("weather info: ", response.data)
+        setWind(response.data.wind.speed)
+        // Convert Kelvin to Celsius
+        setTemp(Math.round(response.data.main.temp - 273.15))
+      })
+  }, [])
+  return(
+    <div>
+      <h1>Wheather in {capital}</h1>
+      <p>temperature {temp} Celucius</p>
+      <p>wind {wind} m/s</p>
+    </div>
+  )
+} 
+
 const CountryInfo = ({onlyCountry}) =>{
   return(
     <>
@@ -20,7 +58,7 @@ const CountryInfo = ({onlyCountry}) =>{
         <ul>
         {Object.values(onlyCountry.languages).map(lang => <li key={lang}>{lang}</li>)}
         </ul>
-        <img src={onlyCountry.flags.png}></img></>
+        <img alt="flag" src={onlyCountry.flags.png}></img></>
   )
 }
 
@@ -41,7 +79,10 @@ const Countries = ({countriesToShow, setShow, show}) =>{
   }else if(countriesToShow.length === 1){
     
     return(
-      <CountryInfo onlyCountry={countriesToShow[0]}/>
+      <div>
+        <CountryInfo onlyCountry={countriesToShow[0]}/>
+        <Weather country={countriesToShow[0]}/>
+      </div>
     )
   }else{
     return(
@@ -67,13 +108,6 @@ const Countries = ({countriesToShow, setShow, show}) =>{
                 </>
                 )
               }
-              <p key={country.name.official}>{country.name.official} 
-              <button onClicked={buttonClicked}>View</button>
-              </p>  
-
-              return <></>
-              
-            
             }
 
            )
